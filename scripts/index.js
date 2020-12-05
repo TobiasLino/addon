e44c51733f590bbf855d43ef85598e916cfb5439
TobiasLino <tobias.lino@sonda.com>
2020-12-05 13:49:04 -0300

    adição de um hello world electron

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,28 @@
-const { add } = require("../build/Release/addon.node")
 const { app, BrowserWindow } = require('electron')
+const { add } = require("../build/Release/addon.node")
+
+const jsAddNum = (num) => {
+    let result = num;
+    for (let i = 0; i < 1000000000; ++i) {
+        result = result + 333;
+    }
+    return result;
+};
+
+function addNum(number) {
+    console.time("C++")
+    console.log(add(number))
+    console.timeEnd("C++")
+    console.log(">>>>>>>>>>>>>>>>>>>>")
+    console.time("JS")
+    console.log(jsAddNum(number))
+    console.timeEnd("JS")
+
+    var location = document.getElementById("calc")
+    var html = "<div>" + add(number) + "</div>";
+
+    location.innerHTML = html;
+}
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -27,4 +50,3 @@ app.on('activate', () => {
     }
 })
 
-console.log(add(2.2))
