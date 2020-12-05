0b70eff7b38b4a31814fe7ac5aa97ae69412ab43
TobiasLino <tobias.lino@sonda.com>
2020-12-05 12:59:30 -0300

    adição do view

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const { add } = require("../build/Release/addon.node")
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('views/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().lenght === 0) {
+        createWindow()
+    }
+})
+
+console.log(add(2.2))
